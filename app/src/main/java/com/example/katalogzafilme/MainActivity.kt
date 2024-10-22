16162c1c86d12e0f093ea6d7c1ebdd80b21eb1ca
zanoixo <62448782+zanoixo@users.noreply.github.com>
2024-10-22 08:47:30 +0200

    testing kotlin

--- app/src/main/java/com/example/katalogzafilme/MainActivity.kt
+++ app/src/main/java/com/example/katalogzafilme/MainActivity.kt
@@ -4,13 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.katalogzafilme.ui.theme.KatalogZaFilmeTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,18 +30,41 @@ class MainActivity : ComponentActivity() {
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
+                    TopText("Katalog filmov")
                 }
             }
+
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun TopText(title : String){
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        text = title
     )
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Column {
+        for (i in 1..3){
+            Row{
+
+                for (j in 1..3) {
+                    Text(
+                        text = "$i box ",
+                        modifier = modifier
+                    )
+                }
+
+
+
+            }
+        }
+    }
+
+
 }
 
 @Preview(showBackground = true)
@@ -43,5 +72,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 fun GreetingPreview() {
     KatalogZaFilmeTheme {
         Greeting("Android")
+
     }
 }
