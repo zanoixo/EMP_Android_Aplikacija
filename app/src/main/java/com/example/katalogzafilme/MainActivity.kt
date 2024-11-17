a9c842c44074fd46afa3fb4957c208041e64fd47
zanoixo <62448782+zanoixo@users.noreply.github.com>
2024-11-17 19:04:20 +0100

    added phase functions

--- app/src/main/java/com/example/katalogzafilme/MainActivity.kt
+++ app/src/main/java/com/example/katalogzafilme/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.katalogzafilme
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -12,6 +13,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
@@ -19,9 +21,14 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import com.example.katalogzafilme.ui.theme.KatalogZaFilmeTheme
 
+private const val TAG = "MyActivity"
+
+
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "On create called")
         enableEdgeToEdge()
         setContent {
             KatalogZaFilmeTheme {
@@ -36,13 +43,48 @@ class MainActivity : ComponentActivity() {
 
         }
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "onRestart")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume")
+    }
 }
+
 
 @Composable
 fun TopText(title : String){
     Text(
         text = title
     )
+}
+
+fun a() {
+    Log.d(TAG, "Button pressed")
 }
 
 @Composable
@@ -52,10 +94,9 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
             Row{
 
                 for (j in 1..3) {
-                    Text(
-                        text = "$i box ",
-                        modifier = modifier
-                    )
+                    TextButton(onClick = {a()}) {
+                        Text("button$j")
+                    }
                 }
 
 
@@ -66,6 +107,8 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 
 
 }
+
+
 
 @Preview(showBackground = true)
 @Composable
